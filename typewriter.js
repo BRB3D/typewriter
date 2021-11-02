d21f26bd49a594a53331f9608d5ec55e0703fee2
BRB3D <samuraix221@hotmail.com>
2021-11-02 15:56:14 -0400

    eslint file of typewriter

--- typewriter.js
+++ typewriter.js
@@ -3,9 +3,9 @@ let delay = 0;
 let amountOfTime = 50;
 for (const char of sentence) {
   setTimeout(() => {
-  process.stdout.write(char);
-}, delay) 
-delay += amountOfTime;
+    process.stdout.write(char);
+  }, delay);
+  delay += amountOfTime;
 }
 let delay2 = sentence.length * amountOfTime  + amountOfTime;
-setTimeout(()=> process.stdout.write('\n'), delay2)
+setTimeout(()=> process.stdout.write('\n'), delay2);
